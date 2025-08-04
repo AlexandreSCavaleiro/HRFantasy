d86110c9f0fdbe61b1c875e93dcef71371803ade
alexandreCavaleiro <alexandre_cavaleiro@outlook.com>
2025-08-04 17:36:24 -0300

    Start testing

---
+++ HRFantasy/Pergunta.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRFantasy
+{
+    internal class Pergunta
+    {
+        string pergunta;
+        string r1, r2, r3, r4;
+        int IDcorreta;
+        string item;
+
+        public Pergunta(string pergunta, string r1, string r2, string r3, string r4, int iDcorreta, string item)
+        {
+            this.pergunta = pergunta;
+            this.r1 = r1;
+            this.r2 = r2;
+            this.r3 = r3;
+            this.r4 = r4;
+            IDcorreta = iDcorreta;
+            this.item = item;
+        }
+
+        public void showAll()
+        {
+            Console.WriteLine($"""
+                    Pergunta:
+                    {pergunta}
+                    
+                    1.{r1}
+                    2.{r2}
+                    3.{r3}
+                    4.{r4}
+
+                    Aperte a tecla referente a resposta correta...
+                """);
+        }
+
+    }
+}
--- HRFantasy/Program.cs
+++ HRFantasy/Program.cs
@@ -1,11 +1,15 @@
 ﻿
+using HRFantasy;
+
 namespace HrFantasy
 {
     class Program
     {
         public static void Main()
         {
+            Pergunta p1 = new Pergunta("seu nome:", " alex", "ale", "xam", "le", 1, "espada longa");
 
+            p1.showAll();
 
         }
     }
