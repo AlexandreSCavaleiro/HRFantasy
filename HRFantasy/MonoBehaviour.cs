a3e74f51f08453e5e64808d064d0f7a61025d2d0
alexandreCavaleiro <alexandre_cavaleiro@outlook.com>
2025-08-06 15:48:51 -0300

    feat: import monoBehavior

---
+++ HRFantasy/MonoBehaviour.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProjetoUC
+{
+    public abstract class MonoBehaviour
+    {
+        //attr
+        private Thread t;
+        private bool rodando = true;
+
+        public bool visible = false;
+        public bool input = false;
+
+        public int speed = 170;
+
+        public void Run()
+        {
+            rodando = true;
+            Awake();
+            Start();
+
+            t = new Thread(
+                () =>
+                {
+                    while (rodando)
+                    {
+                        Update();
+                        LateUpdate();
+                        Thread.Sleep(speed);
+                    }
+
+                }
+            );
+
+            t.Start();
+        }
+
+        public void Stop()
+        {
+            this.rodando = false;
+            OnDestroy();
+            t.Join();
+        }
+
+        public virtual void Awake() { }
+        public virtual void Start() { }
+        public virtual void Update() { }
+        public virtual void LateUpdate() { }
+        public virtual void OnDestroy() { }
+
+
+        public abstract void Draw();
+
+    }
+}
